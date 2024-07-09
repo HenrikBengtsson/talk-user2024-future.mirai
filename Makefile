ac738e2e3d84fe7cdf29590c1ee9c868f3c50902
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-07-10 00:20:57 +0200

    Initial commit with latest state

---
+++ Makefile
@@ -0,0 +1,10 @@
+install-extensions:
+	quarto add quarto-ext/pointer
+	quarto install extension shafayetShafee/bsicons
+#	quarto install extension andrie/reveal-auto-agenda
+
+publish:
+	quarto publish
+
+preview:
+	quarto preview
---
+++ generate-meta.R
@@ -0,0 +1,10 @@
+metathis::meta() |> 
+  metathis::meta_social(
+    title = "future.mirai: Use the Mirai Parallelization Framework in Futureverse - Easy!",
+    description = "A 20-minute talk on the future.mirai package",
+    url = "https://henrikbengtsson.github.io/talk-user2024-future.mirai/#/",
+    image = "https://raw.githubusercontent.com/HenrikBengtsson/talk-user2024-future.mirai/main/images/index-img.png",
+    image_alt = "The title slide of the presentation, with the future and mirai hex logos",
+    twitter_card_type = "summary_large_image",
+    twitter_creator = "@HenrikBengtsson"
+  )
